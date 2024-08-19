8879bfd8784526176b4dacea1ddc21a16b1254bd
EricQyhan <Ericqyhan@gmail.com>
2024-08-18 23:54:02 -0400

    coding

---
+++ script.js
@@ -0,0 +1,41 @@
+// Strengths
+
+const allWods = [strengthWODS];
+
+const strengthWODS = [
+  {
+    name: "TONGUE LASHING",
+    wod: [
+      "(1) - Work up to 1RM Standing Press",
+      "(2) - Six Rounds - 3x Press @ 75-80% 1RM + 10m Walking lunges holding DB",
+      "(3) - Five Rounds - 5x Curtis P's + 1x Rope Climb or 10x Pull Ups",
+      "(4) - 30/30 Jingle Jangles",
+    ],
+  },
+  {
+    name: "BEN FRANKLIN",
+    wod: [
+      "(1) - Five Rounds - 1 Min KB swings @ 53# + 10x Cauldron (10x each side, 20x total @ 30#) + 15x Ankles to bar + 20x GHD Sit-Up + 20x Poor Man's Reverse Hyper",
+    ],
+    "(1)": "5 rounds",
+    "": "1 Min KB swings @ 53#",
+    "": "10x Cauldron (10x each side, 20x total @ 30#)",
+    "": "15x Ankles to bar",
+    "": "20x GHD sit-ups",
+    "": "20x Poor Man’s Reverse Hyper",
+  },
+];
+
+//  OR
+
+// function makeWOD(name, wod) {
+//   this.name = name;
+//   this.wod = wod;
+// }
+
+// let tongueLashing = new makeWOD("TONGUE LASHING", [
+//   "(1) - Work up to 1RM Standing Press",
+//   "(2) - Six Rounds - 3x Press @ 75-80% 1RM + 10m Walking lunges holding DB",
+//   "(3) - Five Rounds - 5x Curtis P's + 1x Rope Climb or 10x Pull Ups",
+//   "(4) - 30/30 Jingle Jangles",
+// ]);
